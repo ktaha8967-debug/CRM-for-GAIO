55eeebb457c0b109ae2dd1e8503dffa2ea677217
ktaha8967 <ktaha8967@gmail.com>
2026-03-13 23:49:30 +0500

    chore: include full origins in allowedDevOrigins for 103.6.169.235

--- next.config.ts
+++ next.config.ts
@@ -3,7 +3,7 @@ import type { NextConfig } from "next";
 const nextConfig: NextConfig = {
   /* config options here */
   experimental: {
-    allowedDevOrigins: ["103.6.169.235"],
+    allowedDevOrigins: ["103.6.169.235", "http://103.6.169.235", "https://103.6.169.235"],
   },
 };
 
