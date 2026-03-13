256813dae10c6908644452158c95b09be0698a15
ktaha8967 <ktaha8967@gmail.com>
2026-03-13 23:46:52 +0500

    chore: allow cross-origin dev request from 103.6.169.235

--- next.config.ts
+++ next.config.ts
@@ -2,6 +2,9 @@ import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
   /* config options here */
+  experimental: {
+    allowedDevOrigins: ["103.6.169.235"],
+  },
 };
 
 export default nextConfig;
